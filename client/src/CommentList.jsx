06f9ae904e7af69e031ee19e9f24baf6a6538455
devil <>
2022-05-09 19:31:59 +0530

    ms

---
+++ client/src/CommentList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    const content =
+      comment.status === "approved"
+        ? comment.content
+        : comment.status === "pending"
+        ? "Comment under moderation"
+        : "Comment rejected";
+
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
---
+++ moderation/index.js
@@ -0,0 +1,26 @@
+const app = require("express")();
+const bodyPareser = require("body-parser");
+const axios = require("axios");
+
+app.use(bodyPareser.json());
+
+app.post("/events", async (req, res) => {
+  if (req.body.type === "CommentCreated") {
+    const { data } = req.body;
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4003, () => console.log("Listening on 4003"));
